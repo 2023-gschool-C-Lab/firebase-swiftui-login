7040c6eb87bd037e2bee3220963ccb70c9789db4
Taekyun Kim <taekkim@student.42seoul.kr>
2023-05-31 16:24:11 +0900

    Initial Commit

---
+++ Firebase+Tutorial/Firebase_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_TutorialApp.swift
+//  Firebase+Tutorial
+//
+//  Created by taekkim on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
